f0ea90b61e8f5ddecaed72b01fc4f3921165aac1
CainIsh <mariusz.machuta@gmail.com>
2015-01-19 11:39:31 +0100

    Dane - new header

---
+++ app/Plugin/Dane/webroot/js/pageHeader.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+    var headerExtend = $('.objectPageHeaderBg.extended');
+
+    if (headerExtend.length) {
+        headerExtend.css('max-height', headerExtend.css('min-height'));
+        headerExtend.css('min-height', Math.floor(parseInt(headerExtend.css('max-height')) - $(window).scrollTop()));
+
+        $(window).scroll(function (event) {
+            var scroll = $(window).scrollTop(),
+                newHeight = Math.floor(parseInt(headerExtend.css('max-height')) - scroll);
+
+            headerExtend.css('min-height', (newHeight > headerExtend.css('max-scroll') ? headerExtend.css('max-scroll') : (newHeight < 0 ? 0 : newHeight + 'px')));
+        });
+    }
+});
