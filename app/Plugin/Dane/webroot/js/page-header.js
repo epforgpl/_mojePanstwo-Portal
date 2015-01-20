1f00db6a191eaf5dc27af19f04d388d3505fbe0e
CainIsh <mariusz.machuta@gmail.com>
2015-01-20 10:52:54 +0100

    Krakow - menu

--- app/Plugin/Dane/webroot/js/page-header.js
+++ app/Plugin/Dane/webroot/js/page-header.js
@@ -3,11 +3,11 @@ jQuery(document).ready(function () {
 
     if (headerExtend.length) {
         headerExtend.css('max-height', headerExtend.css('min-height'));
-        headerExtend.css('min-height', Math.floor(parseInt(headerExtend.css('max-height')) - $(window).scrollTop()));
+        headerExtend.css('min-height', Math.floor(parseInt(headerExtend.css('max-height')) - ($(window).scrollTop() * .5)));
 
         $(window).scroll(function (event) {
             var scroll = $(window).scrollTop(),
-                newHeight = Math.floor(parseInt(headerExtend.css('max-height')) - scroll);
+                newHeight = Math.floor(parseInt(headerExtend.css('max-height')) - (scroll * .5));
 
             headerExtend.css('min-height', (newHeight > headerExtend.css('max-scroll') ? headerExtend.css('max-scroll') : (newHeight < 0 ? 0 : newHeight + 'px')));
         });
