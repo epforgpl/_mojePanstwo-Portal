fb2a21d87b4963b29d64ea8a72ba66f8822cbb2b
unknown <marekbielecki93@gmail.com>
2015-01-21 14:55:09 +0100

    handel_zagraniczny_towary widok

--- app/Plugin/Dane/webroot/js/view-handel-zagraniczny-towary.js
+++ app/Plugin/Dane/webroot/js/view-handel-zagraniczny-towary.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var apiHost = 'http://mojepanstwo.pl:4444/';
 
     for(var i = 0; i < data.length; i++) {
-        $('#selectYear').append('<option>' + data[i].rocznik + '</option>');
+        $('#selectYear').append('<option value="' + data[i].rocznik + '">' + data[i].rocznik + '</option>');
         categories.push(parseInt(data[i].rocznik));
         seriesImportData.push(parseInt(data[i].import_pln));
         seriesExportData.push(parseInt(data[i].eksport_pln));
@@ -58,14 +58,14 @@ $(document).ready(function() {
     });
 
     var loadTopData = function(year) {
-        $.getJSON(apiHost + 'handel_zagraniczny/stats/towary.json?panstwo_id=' + _objectData.id + '&rocznik=' + year + '&order=import', function( data ) {
+        $.getJSON(apiHost + 'handel_zagraniczny/stats/panstwa.json?symbol_id=' + _objectData.id + '&rocznik=' + year + '&order=import', function( data ) {
             $('#topImportList').html('');
             $.each(data, function(key, val) {
                 $('#topImportList').append('<li class="list-group-item"><span class="badge">' + val.wartosc_pln + ' zł</span>' + val.nazwa + '</li>');
             });
         });
 
-        $.getJSON(apiHost + 'handel_zagraniczny/stats/towary.json?panstwo_id=' + _objectData.id + '&rocznik=' + year + '&order=eksport', function( data ) {
+        $.getJSON(apiHost + 'handel_zagraniczny/stats/panstwa.json?symbol_id=' + _objectData.id + '&rocznik=' + year + '&order=eksport', function( data ) {
             $('#topExportList').html('');
             $.each(data, function(key, val) {
                 $('#topExportList').append('<li class="list-group-item"><span class="badge">' + val.wartosc_pln + ' zł</span>' + val.nazwa + '</li>');
@@ -78,6 +78,7 @@ $(document).ready(function() {
         loadTopData(year);
     });
 
+    $('#selectYear').val('2014');
     loadTopData(2005); // default
 
 });
