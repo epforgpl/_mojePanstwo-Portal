bfe08498a4f192943fab15f3f6e7ca4d4e0a6fcc
emaeraes <marekbielecki93@gmail.com>
2015-02-10 15:08:48 +0100

    nowy widok hz - panstwa

--- app/Plugin/Dane/webroot/js/view-panstwa.js
+++ app/Plugin/Dane/webroot/js/view-panstwa.js
@@ -5,11 +5,24 @@ $(document).ready(function() {
     var series;
     var seriesImportData = [];
     var seriesExportData = [];
+    var seriesImport2014Data = [];
+    var seriesExport2014Data = [];
     var apiHost = 'http://mojepanstwo.pl:4444/';
 
     for(var i = 0; i < data.length; i++) {
         $('#selectYear').append('<option value="' + data[i].rocznik + '">' + data[i].rocznik + '</option>');
         categories.push(parseInt(data[i].rocznik));
+        if(parseInt(data[i].rocznik) === 2014 || parseInt(data[i].rocznik) === 2013) {
+            seriesImport2014Data.push(parseInt(data[i].import_pln));
+            seriesExport2014Data.push(parseInt(data[i].eksport_pln));
+            if(parseInt(data[i].rocznik) === 2013) {
+                seriesImportData.push(parseInt(data[i].import_pln));
+                seriesExportData.push(parseInt(data[i].eksport_pln));
+            }
+            continue;
+        }
+        seriesImport2014Data.push(null);
+        seriesExport2014Data.push(null);
         seriesImportData.push(parseInt(data[i].import_pln));
         seriesExportData.push(parseInt(data[i].eksport_pln));
     }
@@ -22,6 +35,18 @@ $(document).ready(function() {
         {
             name: 'Eksport',
             data: seriesExportData
+        },
+        {
+            name: 'Import (dane wstępne)',
+            dashStyle: 'shortdot',
+            data: seriesImport2014Data,
+            color: '#7cb5ec'
+        },
+        {
+            name: 'Export (dane wstępne)',
+            dashStyle: 'shortdot',
+            data: seriesExport2014Data,
+            color: '#434348'
         }
     ];
 
@@ -78,11 +103,12 @@ $(document).ready(function() {
 
     $('#selectYear').change(function() {
         var year = $(this).find("option:selected").text();
-        loadTopData(year);
+        // loadTopData(year);
+        tree.setYear(year);
     });
 
     $('#selectYear').val(_year);
-    loadTopData(parseInt(_year)); // default
+    // loadTopData(parseInt(_year)); // default
 
     function pl_currency_format(n) {
         var str = '';
@@ -146,24 +172,179 @@ $(document).ready(function() {
         return s.join(dec);
     }
 
-    $('#tree1').tree({
-        data: [
-            {
-                label: 'node1', id: 1,
-                children: [
-                    { label: 'child1', id: 2 },
-                    { label: 'child2', id: 3 }
-                ]
-            },
-            {
-                label: 'node2', id: 4,
-                children: [
-                    { label: 'child3', id: 5 }
-                ]
+    var tree = {
+
+        $import: $('#import'),
+        import_data: [],
+
+        $export: $('#export'),
+        export_data: [],
+
+        country_id: 0,
+        year: 0,
+
+        setYear: function(year) {
+            this.year = parseInt(year);
+            var k = this.year + '_0';
+            var t = this;
+            if(t.import_data[k] === undefined) {
+                t.get(t.year, 'import', 0, function(data) {
+                    t.import_data[k] = data;
+                    t.get(t.year, 'export', 0, function(data) {
+                        t.export_data[k] = data;
+                        t.update();
+                    });
+                });
+            } else {
+                t.update();
             }
-        ],
-        autoOpen: true,
-        dragAndDrop: true
-    });
+        },
+
+        update: function() {
+            var k = this.year + '_0';
+            this.$import.html('');
+            for(var i = 0; i < this.import_data[k].length; i++) {
+                var row = this.import_data[k][i];
+                this.$import.append(
+                    this.template(row)
+                );
+            }
+            this.$export.html('');
+            for(var i = 0; i < this.export_data[k].length; i++) {
+                var row = this.export_data[k][i];
+                this.$export.append(
+                    this.template(row)
+                );
+            }
+
+            var t = this;
+
+            $('#import li').click(function() {
+                if($(this).hasClass('open')) {
+                    $(this).removeClass('open');
+                    $(this).find('ul').remove();
+                    return false;
+                }
+                $(this).addClass('open');
+                var parent_id = parseInt($(this).attr('id'));
+                var k = t.year + '_' + parent_id;
+                if(t.import_data[k] === undefined) {
+                    t.get(t.year, 'import', parent_id, function(data) {
+                        t.import_data[k] = data;
+                        t.update_childs('import', parent_id);
+                    });
+                } else {
+                    t.update_childs('import', parent_id);
+                }
+            });
+
+            $('#export li').click(function() {
+                if($(this).hasClass('open')) {
+                    $(this).removeClass('open');
+                    $(this).find('ul').remove();
+                    return false;
+                }
+                $(this).addClass('open');
+                var parent_id = parseInt($(this).attr('id'));
+                var k = t.year + '_' + parent_id;
+                if(t.export_data[k] === undefined) {
+                    t.get(t.year, 'export', parent_id, function(data) {
+                        t.export_data[k] = data;
+                        t.update_childs('export', parent_id);
+                    });
+                } else {
+                    t.update_childs('export', parent_id);
+                }
+            });
+        },
+
+        update_childs: function(type, parent_id) {
+            var k = this.year + '_' + parent_id;
+            switch(type) {
+                default: // import
+                    var t = this;
+                    $('#import #' + parent_id).find('ul').remove();
+                    if(this.import_data[k].length === 0) {
+                        return false;
+                    }
+                    $('#import #' + parent_id).append('<ul></ul>');
+                    for(var i = 0; i < this.import_data[k].length; i++) {
+                        var row = this.import_data[k][i];
+                        $('#import #' + parent_id + ' ul').append(this.template(row));
+                    }
+                    $('#import #' + parent_id + ' ul li').click(function() {
+                        if($(this).hasClass('open')) {
+                            $(this).removeClass('open');
+                            $(this).find('ul').remove();
+                            return false;
+                        }
+                        $(this).addClass('open');
+                        var parent_id = parseInt($(this).attr('id'));
+                        var k = t.year + '_' + parent_id;
+                        if(t.import_data[k] === undefined) {
+                            t.get(t.year, 'import', parent_id, function(data) {
+                                t.import_data[k] = data;
+                                t.update_childs('import', parent_id);
+                            });
+                        } else {
+                            t.update_childs('import', parent_id);
+                        }
+                        return false;
+                    });
+                break;
+
+                case 'export':
+                    var t = this;
+                    $('#export #' + parent_id).find('ul').remove();
+                    if(this.export_data[k].length === 0) {
+                        return false;
+                    }
+                    $('#export #' + parent_id).append('<ul></ul>');
+                    for(var i = 0; i < this.export_data[k].length; i++) {
+                        var row = this.export_data[k][i];
+                        $('#export #' + parent_id + ' ul').append(this.template(row));
+                    }
+                    $('#export #' + parent_id + ' ul li').click(function() {
+                        if($(this).hasClass('open')) {
+                            $(this).removeClass('open');
+                            $(this).find('ul').remove();
+                            return false;
+                        }
+                        $(this).addClass('open');
+                        var parent_id = parseInt($(this).attr('id'));
+                        var k = t.year + '_' + parent_id;
+                        if(t.export_data[k] === undefined) {
+                            t.get(t.year, 'export', parent_id, function(data) {
+                                t.export_data[k] = data;
+                                t.update_childs('export', parent_id);
+                            });
+                        } else {
+                            t.update_childs('export', parent_id);
+                        }
+                        return false;
+                    });
+                break;
+            }
+        },
+
+        template: function(row) {
+            return '<li id="' + row.id + '"><span class="currency">' + pl_currency_format(row.wartosc_pln) + ' zł</span><p>' + row.nazwa + ' <a href="/dane/handel_zagraniczny_towary/' + row.id + '" title="Zobacz szczegóły dotyczące tego towaru">więcej</a></p></li>';
+        },
+
+        get: function(year, type, parent_id, done_function) {
+            $.getJSON(
+                apiHost + 'handel_zagraniczny/stats/getSymbols.json' +
+                '?parent_id=' + parent_id +
+                '&year=' + year +
+                '&type=' + type +
+                '&country_id=' + this.country_id +
+                '&limit=9999',
+                done_function
+            );
+        }
+    };
+
+    tree.country_id = parseInt(_objectData.id);
+    tree.setYear(_year);
 
 });
