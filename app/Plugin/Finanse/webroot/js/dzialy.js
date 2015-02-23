757d6b78f938f896d1575b5333c558acb876e830
emaeraes <marekbielecki93@gmail.com>
2015-02-23 11:57:24 +0100

    finanse - zestawienie dla wyszukanej gminy

--- app/Plugin/Finanse/webroot/js/dzialy.js
+++ app/Plugin/Finanse/webroot/js/dzialy.js
@@ -25,6 +25,52 @@ function number_format(number, decimals, dec_point, thousands_sep) {
     return s.join(dec);
 }
 
+function pl_currency_format(n) {
+    var str = '';
+    var mld = 0;
+    var mln = 0;
+    var tys = 0;
+
+    if(n > 1000000000) {
+        mld = Math.round(n / 1000000000, 2);
+        n -= mld * 1000000000;
+        return mld + ' Mld';
+    }
+
+    if(n > 1000000) {
+        mln = Math.round(n / 1000000, 2);
+        n -= mln * 1000000;
+        return mln + ' M';
+    }
+
+    if(n > 1000) {
+        tys = Math.round(n / 1000, 2);
+        n -= tys * 1000;
+        return tys + ' k';
+    }
+
+    if(mld > 0)
+        str += mld + ' Mld ';
+    if(mln  > 0)
+        str += mln + ' M ';
+    if(tys > 0 && mld === 0)
+        str += tys + ' k';
+
+    if(mld === 0 && mln === 0 && tys === 0)
+        str += number_format(n);
+
+    return str.trim();
+};
+
+
+var zakresy = [
+    [0, 20000],
+    [20000, 50000],
+    [50000, 100000],
+    [100000, 500000],
+    [500000, 999999999]
+];
+
 $(document).ready(function() {
     'use strict';
 
@@ -151,33 +197,129 @@ $(document).ready(function() {
                 return false;
 
             $("#teryt_search_input").val(ui.item.name);
+            $('#gmina_alert p').html('');
+            $('#gmina_alert .close').hide();
+            $('#gmina_alert p').addClass('loading');
+            $('#gmina_alert').show();
+            $('#sections').hide();
 
             $.getJSON(api_url + 'finanse/finanse/getCommuneData?id=' + ui.item.value, function(data) {
+                $('#gmina_alert p').removeClass('loading');
+                $('#gmina_alert .close').show();
 
                 if(!data || data.length === 0) {
                     $('#section_' + _dzial_id + '_addon').css('display', 'none');
+                    $('#gmina_alert p').html('Wystąpił błąd. Spróbuj ponownie za kilka minut.');
                     return false;
                 }
 
-                for(var i = 0; i < data.length; i++) {
-                    var _dzial_id = parseInt(data[i].dzial_id);
-                    var _sum_wydatki = parseInt(data[i].sum_wydatki);
-                    $('#section_' + _dzial_id + '_addon').css('display', 'block');
-                    $('#section_' + _dzial_id + '_addon .n').html(ui.item.name);
-                    $('#section_' + _dzial_id + '_addon .v').html(number_format(_sum_wydatki, 0, '.', ' '));
-                    // calc left
-                    var _min = parseInt($('#section_' + _dzial_id + ' ul.addons .min').data('int'));
-                    var _max = parseInt($('#section_' + _dzial_id + ' ul.addons .max').data('int'));
-                    var _left = parseInt((_sum_wydatki / (_max - _min)) * 100);
-                    $('#section_' + _dzial_id + '_addon').css('left', _left + '%');
+                var zakres = zakresy[parseInt(data.sections[0].zakres)];
+                $('#gmina_alert p').html('Zestawienie wydatków gminy ' + ui.item.name + ' z innymi gminami o liczbie mieszkańców z przedziału od ' + zakres[0] + ' do ' + zakres[1]);
+
+                for(var i = 0; i < data.sections.length; i++) {
+                    var d = data.sections[i];
+                    var e = $('#dsection_' + d.id);
+                    e.find('p.dsum').html(pl_currency_format(d.sum_wydatki));
+                    e.find('ul.addons li.min .n').html(d.min_nazwa);
+                    e.find('ul.addons li.min .v').html(number_format(d.min, 0, '.', ' '));
+                    e.find('ul.addons li.max .n').html(d.max_nazwa);
+                    e.find('ul.addons li.max .v').html(number_format(d.max, 0, '.', ' '));
+                    e.find('ul.addons li.section_addon .n').html(ui.item.name);
+                    e.find('ul.addons li.section_addon .v').html(number_format(d.commune, 0, '.', ' '));
+
+                    var _min = parseInt(d.min);
+                    var _sum_wydatki = parseInt(d.commune);
+                    var _max =  parseInt(d.max);
+                    if(_sum_wydatki <= _min)
+                        var _left = 0;
+                    else
+                        var _left = parseInt(((_sum_wydatki - _min) / (_max - _min)) * 100);
+
+                    e.find('ul.addons li.section_addon').css('left', _left + '%');
+
+                    var charts_data = [];
+                    for(var s = 0; s < d.buckets.length; s++) {
+                        charts_data.push(Number(d.buckets[s].height));
+                    }
+
+                    chart = new Highcharts.Chart({
+                        chart: {
+                            renderTo: 'dhistogram_' + d.id,
+                            type: 'column',
+                            height: 150,
+                            backgroundColor: null,
+                            spacingTop: 0
+                        },
+
+                        tooltip: {
+                            enabled: false
+                        },
+
+                        credits: {
+                            enabled: false
+                        },
+
+                        legend: {
+                            enabled: false
+                        },
+
+                        title: {
+                            text: ''
+                        },
+
+                        xAxis: {
+                            labels: {
+                                enabled: false
+                            },
+                            gridLineWidth: 0,
+                            title: null
+                        },
+
+                        yAxis: {
+                            labels: {
+                                enabled: false
+                            },
+                            gridLineWidth: 0,
+                            title: {
+                                text: 'Liczba gmin',
+                                style: {
+                                    color: '#AAA',
+                                    'font-family': '"Helvetica Neue",Helvetica,Arial,sans-serif',
+                                    'font-size': '13px',
+                                    'font-weight': '300'
+                                }
+                            }
+                        },
+
+                        plotOptions: {
+                            column: {
+                                groupPadding: 0,
+                                pointPadding: 0,
+                                borderWidth: 0
+                            }
+                        },
+
+                        series: [{
+                            data: charts_data
+                        }]
+
+                    });
                 }
 
-                $(this).data("id")
-
+                $('#dsections').show();
             });
 
             return false;
         }
     }).autocomplete("widget").addClass("autocompleteFinanseDzialy");
+
+    // alert close event test
+    $('#gmina_alert .close').on('click', function () {
+        $("#teryt_search_input").val('');
+        $('#dsections').hide();
+        $('#sections').show();
+        $('#gmina_alert').hide();
+        return false;
+    });
 
 });
