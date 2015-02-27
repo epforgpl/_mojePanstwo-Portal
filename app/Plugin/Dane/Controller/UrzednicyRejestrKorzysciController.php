36212d16399a80c28d93bcdba0ed00da9e437a1d
KrzysztofMadejski <krzysztof@madejscy.pl>
2015-02-27 15:19:14 +0100

    Urzednik lista korzysci

---
+++ app/Plugin/Dane/Controller/UrzednicyRejestrKorzysciController.php
@@ -0,0 +1,27 @@
+<?php
+
+App::uses('DataobjectsController', 'Dane.Controller');
+
+class UrzednicyRejestrKorzysciController extends DataobjectsController
+{
+    public $menu = array();
+    public $initLayers = array('html');
+
+    public function view()
+    {
+        parent::_prepareView();
+
+        $dokument_id = $this->object->getData('urzednicy_rejestr_korzysci.dokument_id');
+        $html = $this->object->getLayer('html');
+
+        if ($dokument_id) {
+            $this->set('content_document', $this->API->document($dokument_id));
+
+        } else if ($html) {
+            $this->set('content_html', $html);
+
+        } else {
+            throw new Exception("Either pdf or html should be set for " . $this->request->url);
+        }
+    }
+}
