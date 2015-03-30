1b5aaf0253ff29eb09e5ebd25f4b73a6b420afe1
MarekBielecki <marekbielecki93@gmail.com>
2015-03-30 14:21:06 +0200

    statystyka - finanse gmin

--- app/Plugin/Statystyka/Controller/StatystykaController.php
+++ app/Plugin/Statystyka/Controller/StatystykaController.php
@@ -4,6 +4,10 @@ App::uses('ApplicationsController', 'Controller');
 class StatystykaController extends ApplicationsController
 {
 
+    public $uses = array(
+        'Statystyka.Finanse'
+    );
+
 	public $settings = array(
 		'menu' => array(
 			array(
@@ -24,6 +28,10 @@ class StatystykaController extends ApplicationsController
 					),
 				),
 			),
+            array(
+                'id' => 'finanse_gmin',
+                'label' => 'Finanse gmin'
+            )
 		),
 		'title' => 'Statystyka',
 		'subtitle' => 'Dane statystyczne o Polsce',
@@ -44,6 +52,12 @@ class StatystykaController extends ApplicationsController
     public function bdl_grupy()
     {
         $this->loadDatasetBrowser('bdl_wskazniki_grupy');
+    }
+
+    public function finanse_gmin()
+    {
+        $data = $this->Finanse->getBudgetData();
+        $this->set('data', $data);
     }
 
 } 
---
+++ app/Plugin/Statystyka/Model/Finanse.php
@@ -0,0 +1,13 @@
+<?
+
+class Finanse extends AppModel
+{
+
+    public $useDbConfig = 'mpAPI';
+
+    public function getBudgetData() {
+        $data = $this->getDataSource()->request('finanse/getBudgetData');
+        return $data;
+    }
+
+}
