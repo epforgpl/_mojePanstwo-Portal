ae7d15d67d86294942e04bc82e86466e7cd42fae
MarekBielecki <marekbielecki93@gmail.com>
2015-03-31 14:15:46 +0200

    OAuth tests remove

--- app/Plugin/OAuth/Controller/Component/OAuthComponent.php
+++ app/Plugin/OAuth/Controller/Component/OAuthComponent.php
@@ -501,9 +501,6 @@ class OAuthComponent extends Component implements IOAuth2Storage, IOAuth2Refresh
             'scope' => $scope
         );
 
-        var_dump($data);
-        exit(); die();
-
         return $this->AccessToken->save(array('AccessToken' => $data));
     }
 
