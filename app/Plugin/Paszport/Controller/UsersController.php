e95ea0952c45787e117402cc7498f2f8dd0a29f4
MarekBielecki <marekbielecki93@gmail.com>
2015-04-09 12:19:59 +0200

    fb login cdn

--- app/Plugin/Paszport/Controller/UsersController.php
+++ app/Plugin/Paszport/Controller/UsersController.php
@@ -146,16 +146,14 @@ class UsersController extends PaszportAppController
             // Trzeba wszystkie wywołania $this->PaszportApi zmienić na odwołania do nowego API przez model User
             $_user = new User();
             $user = $_user->findFacebookUser($user_data['id'], $user_data['email']);
-            var_dump($user);
-            die();
-
             if (!isset($user['user']) || empty($user['user'])) {
                 $user = false;
             } else {
                 $user = $user['user'];
             }
-            if ($user && $user['User']['facebook_id']) { # if user is already FB connected to us, just log him in
-                $this->Auth->login($user['User']);
+            if ($user && $user['user']['facebook_id']) { # if user is already FB connected to us, just log him in
+                $user['user']['password'] = md5($user_data['id'] . $user_data['email']);
+                $this->Auth->login($user['user']);
                 $this->_log(array(
                     'msg' => 'LC_PASZPORT_LOG_LOGIN_FB',
                     'ip' => $this->request->clientIp(),
