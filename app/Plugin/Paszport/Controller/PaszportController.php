0978be476eff89c57b0b9e354c4ea2bb6eef96a3
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2015-04-22 07:59:07 +0200

    Usunięcie opcji "Nowe hasło" z menu Paszportu

--- app/Plugin/Paszport/Controller/PaszportController.php
+++ app/Plugin/Paszport/Controller/PaszportController.php
@@ -17,11 +17,6 @@ class PaszportController extends ApplicationsController
 				'label' => 'Zarejestruj',
                 'href' => 'register'
 			),
-            array(
-                'id' => 'forgot',
-                'label' => 'Nowe hasło',
-                'href' => 'forgot'
-            ),
 		),
 		'title' => 'Paszport',
 		'subtitle' => '',
