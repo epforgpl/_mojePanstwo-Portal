ece289e785d37ea76a6336b7f70e126df3ddecea
KrzysztofMadejski <krzysztof@madejscy.pl>
2015-04-30 16:37:35 +0200

    Killing feature that php renders js and css files: https://github.com/cakephp/cakephp/issues/2316

--- lib/Cake/Routing/Filter/AssetDispatcher.php
+++ lib/Cake/Routing/Filter/AssetDispatcher.php
@@ -168,11 +168,11 @@ class AssetDispatcher extends DispatcherFilter {
 		$response->cache( filemtime( $assetFile ) );
 		$response->send();
 		ob_clean();
-		if ( $ext === 'css' || $ext === 'js' ) {
-			include $assetFile;
-		} else {
+//		if ( $ext === 'css' || $ext === 'js' ) {
+//			include $assetFile;
+//		} else {
 			readfile( $assetFile );
-		}
+//		}
 
 		if ( $compressionEnabled ) {
 			ob_end_flush();
