19946f948fbe53a78dbffd98de9d8408810b39c0
CainIsh <mariusz.machuta@gmail.com>
2015-04-30 11:33:22 +0200

    #447 zmiana linkow bez href na zwykły text

--- app/Plugin/Dane/webroot/js/page-header.js
+++ app/Plugin/Dane/webroot/js/page-header.js
@@ -1,25 +1,32 @@
-jQuery(document).ready(function () {
-    var headerExtend = $('.objectPageHeaderBg.extended');
+/*global $,jQuery*/
 
-    if (headerExtend.length) {
+jQuery(document).ready(function () {
+    "use strict";
+    var headerExtend = $('.objectPageHeaderBg.extended'),
+        mobileMenu = $('.mobileMenu');
+
+    if (headerExtend && headerExtend.length) {
         headerExtend.css('max-height', headerExtend.css('min-height'));
-        headerExtend.css('min-height', Math.floor(parseInt(headerExtend.css('max-height')) - ($(window).scrollTop() * .5)));
+        headerExtend.css('min-height', Math.floor(parseInt(headerExtend.css('max-height'), 10) - ($(window).scrollTop() * 0.5)));
 
         $(window).scroll(function () {
             var scroll = $(window).scrollTop(),
-                newHeight = Math.floor(parseInt(headerExtend.css('max-height')) - (scroll * .5));
+                newHeight = Math.floor(parseInt(headerExtend.css('max-height'), 10) - (scroll * 0.5));
 
             headerExtend.css('min-height', (newHeight > headerExtend.css('max-scroll') ? headerExtend.css('max-scroll') : (newHeight < 0 ? 0 : newHeight + 'px')));
         });
     }
 
-    $('.mobileMenu > a').click(function (e) {
-        var menu = $(this).parents('ul.nav');
+    if (mobileMenu && mobileMenu.length) {
+        mobileMenu.find(' > a').click(function (e) {
+            var menu = $(this).parents('ul.nav');
 
-        e.preventDefault();
-        if (menu.hasClass('full'))
-            menu.removeClass('full');
-        else
-            menu.addClass('full');
-    })
+            e.preventDefault();
+            if (menu.hasClass('full')) {
+                menu.removeClass('full');
+            } else {
+                menu.addClass('full');
+            }
+        });
+    }
 });
