c4ee529bf0c0b5145d7ad89853f39194bc79ee55
CainIsh <mariusz.machuta@gmail.com>
2015-04-30 10:37:07 +0200

    #435 zmiana w kolejnosci deklaracji google map + sprawdzanie przed inicjalizacją czy obiekt google map ma być w ogóle uruchamiany + sprawdzanwie czy JS google.map nie jest już załadowany

--- app/Plugin/Dane/webroot/js/view-gminy-krakow.js
+++ app/Plugin/Dane/webroot/js/view-gminy-krakow.js
@@ -1,34 +1,64 @@
+/*global $,jQuery,mPHeart,google*/
+
 var map;
 
-function initialize() {
+function initMap() {
+    "use strict";
     var mapOptions = {
-        zoom: 11,
-        draggable: true,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        navigationControl: true,
-        mapTypeControl: false,
-        scaleControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+            zoom: 11,
+            draggable: true,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            navigationControl: true,
+            mapTypeControl: false,
+            scaleControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        kmlOptions,
+        kmlUrl,
+        kmlLayer,
+        text;
+
     map = new google.maps.Map(document.getElementById('dzielnice_map'), mapOptions);
 
-    var kmlUrl = 'http://mojepanstwo.pl/files/dzielnice_administracyjne.kml';
-    var kmlOptions = {
+    kmlUrl = 'http://mojepanstwo.pl/files/dzielnice_administracyjne.kml';
+    kmlOptions = {
         suppressInfoWindows: true,
         preserveViewport: false,
         map: map
     };
 
-    var kmlLayer = new google.maps.KmlLayer(kmlUrl, kmlOptions);
+    kmlLayer = new google.maps.KmlLayer(kmlUrl, kmlOptions);
 
     google.maps.event.addListenerOnce(map, 'idle', function () {
         map.setZoom(11);
     });
 
     google.maps.event.addListener(kmlLayer, 'click', function (kmlEvent) {
-        var text = kmlEvent.featureData;
+        text = kmlEvent.featureData;
     });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+    "use strict";
+    google.maps.event.addDomListener(window, 'load', initMap);
+}
+
+function loadScript() {
+    "use strict";
+    if (google.maps) {
+        initialize();
+    } else {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=' + mPHeart.language.twoDig + '&' + 'callback=initialize';
+        document.body.appendChild(script);
+    }
+}
+
+$(document).ready(function () {
+    "use strict";
+    if ($('dzielnice_map').length) {
+        window.onload = loadScript();
+    }
+});
