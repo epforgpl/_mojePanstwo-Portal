a18529b33d1995cecd5232fe6ac41ba48ed16aee
Cain-Ish <mariusz.machuta@gmail.com>
2015-06-20 10:01:22 +0200

    pisma preinit adresat

--- app/Plugin/Pisma/webroot/js/pisma.js
+++ app/Plugin/Pisma/webroot/js/pisma.js
@@ -95,6 +95,9 @@ var PISMA = Class.extend({
                 if (preinit.szablon_id) {
                     self.szablonData(preinit.szablon_id);
                 }
+                if (preinit.adresat_id) {
+                    self.adresatData(preinit.adresat_id);
+                }
                 self.html.stepper_div.removeData('pismo');
             }
         }
@@ -197,6 +200,21 @@ var PISMA = Class.extend({
                     self.html.szablony.find('> label').popover('destroy');
                 });
             }
+        });
+    },
+    adresatData: function (adresat_id) {
+        "use strict";
+        var self = this;
+
+        /*$.getJSON("/dane/suggest.json?dataset[]=pisma_adresaci&q=" + adresat_id, function (d) {*/
+        $.getJSON("/dane/suggest.json?dataset[]=pisma_adresaci&q=" + $('#editor-cont .control.control-addressee > p:first').text(), function (d) {
+            self.objects.adresaci = {
+                id: d.options[0].payload.subid,
+                dataset: d.options[0].payload.subdataset,
+                title: d.options[0].text
+            };
+
+            self.html.adresaci.find('.suggesterBlockPisma input.form-control').val(self.objects.adresaci.title);
         });
     },
     editor: function () {
