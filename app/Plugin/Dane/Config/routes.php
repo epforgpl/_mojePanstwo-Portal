1d9bc742cd9672d32f4a9977ee13fc15fe4f30ab
MarekBielecki <marekbielecki93@gmail.com>
2015-06-26 11:26:16 +0200

    ObjectPagesManagement

--- app/Plugin/Dane/Config/routes.php
+++ app/Plugin/Dane/Config/routes.php
@@ -74,12 +74,17 @@ Router::connect( '/dane/:controller/:slug/:id', array(
 ), array( 'id' => '[0-9]+' ) );
 */
 
-
+# ObjectUsersManagement
 Router::connect('/dane/:dataset/:object_id/users/index', array('plugin' => 'Dane', 'controller' => 'ObjectUsersManagement', 'action' => 'index', '[method]' => 'GET'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
 Router::connect('/dane/:dataset/:object_id/users/index', array('plugin' => 'Dane', 'controller' => 'ObjectUsersManagement', 'action' => 'add', '[method]' => 'POST'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
 Router::connect('/dane/:dataset/:object_id/users/:user_id', array('plugin' => 'Dane', 'controller' => 'ObjectUsersManagement', 'action' => 'edit', '[method]' => 'PUT'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+', 'user_id' => '[0-9]+'));
 Router::connect('/dane/:dataset/:object_id/users/:user_id', array('plugin' => 'Dane', 'controller' => 'ObjectUsersManagement', 'action' => 'delete', '[method]' => 'DELETE'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+', 'user_id' => '[0-9]+'));
 
+# ObjectPagesManagement
+Router::connect('/dane/:dataset/:object_id/page/logo', array('plugin' => 'Dane', 'controller' => 'ObjectPagesManagement', 'action' => 'setLogo', '[method]' => 'POST'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
+Router::connect('/dane/:dataset/:object_id/page/logo', array('plugin' => 'Dane', 'controller' => 'ObjectPagesManagement', 'action' => 'deleteLogo', '[method]' => 'DELETE'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
+Router::connect('/dane/:dataset/:object_id/page/cover', array('plugin' => 'Dane', 'controller' => 'ObjectPagesManagement', 'action' => 'setCover', '[method]' => 'POST'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
+Router::connect('/dane/:dataset/:object_id/page/cover', array('plugin' => 'Dane', 'controller' => 'ObjectPagesManagement', 'action' => 'deleteCover', '[method]' => 'DELETE'), array('dataset' => '([a-zA-Z\_]+)', 'object_id' => '[0-9]+'));
 
 Router::connect('/dane', array('plugin' => 'Dane', 'controller' => 'Dane', 'action' => 'view'));
 
--- app/Plugin/Dane/Model/ObjectPagesManagement.php
+++ app/Plugin/Dane/Model/ObjectPagesManagement.php
@@ -1,11 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 24.06.15
- * Time: 16:10
- */
 
-class ObjectPagesManagement {
+class ObjectPagesManagement extends AppModel {
+
+    public $useDbConfig = 'mpAPI';
+
+    private $request;
+
+    public function setRequest($request) {
+        $this->request = $request;
+    }
+
+    public function setLogo() {
+        return $this->getResponse('setLogo', array(
+            'method' => 'POST'
+        ));
+    }
+
+    public function deleteLogo() {
+        return $this->getResponse('deleteLogo', array(
+            'method' => 'DELETE'
+        ));
+    }
+
+    public function setCover() {
+        return $this->getResponse('setCover', array(
+            'method' => 'POST'
+        ));
+    }
+
+    public function deleteCover() {
+        return $this->getResponse('deleteCover', array(
+            'method' => 'DELETE'
+        ));
+    }
+
+    public function isEditable() {
+        $res =  $this->getResponse('isEditable', array(
+            'method' => 'POST'
+        ));
+
+        return ($res == '1');
+    }
+
+    private function getResponse($endpoint, $options) {
+        return $this->getDataSource()->request('dane/' . $this->request['dataset'] . '/' . $this->request['object_id'] . '/pages/' . $endpoint . '.json', $options);
+    }
 
 }
