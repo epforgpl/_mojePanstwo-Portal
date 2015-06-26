7b8fce3f3bb8560a6e0f2ac2c7f668f99f4b363b
McCease <maxiz@wp.pl>
2015-06-26 18:24:06 +0200

    BDL temp indicators

--- app/Plugin/Bdl/Model/BdlTempItem.php
+++ app/Plugin/Bdl/Model/BdlTempItem.php
@@ -4,21 +4,39 @@ class BdlTempItem extends AppModel
 {
 
     public $useTable = false;
-    
-    public function find() {
-	    
+
+    public function find($type = 'first', $params = array())
+    {
+        $ret=null;
+        switch($type){
+            case 'first':{
+                break;
+            }
+            case 'all':{
+                break;
+            }
+            case 'list':{
+                break;
+            }
+            case 'count':{
+                break;
+            }
+        }
     }
-    
-    public function findById($id) {
-	    
+
+    public function findById($id)
+    {
+        return $this->find('first', array('conditions'=>array('id'=>$id)));
     }
-    
-    public function save() {
-	    
+
+    public function save()
+    {
+
     }
-    
-    public function delete() {
-	    
+
+    public function delete()
+    {
+
     }
-    
+
 }
