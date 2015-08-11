e1f0848a3038219368bb63788cfd1849c745c077
CainIsh <mariusz.machuta@gmail.com>
2015-08-11 10:05:49 +0200

    cropit update 0.4.1

--- app/webroot/plugins/cropit/update_version.js
+++ app/webroot/plugins/cropit/update_version.js
@@ -1,6 +1,6 @@
 #! /usr/local/bin/node
 
-var sys = require('sys');
+var sys = require('sys')
 var exec = require('child_process').exec;
 var fs = require('fs');
 
@@ -8,14 +8,12 @@ var version = require('./cropit.jquery.json').version;
 
 sys.puts('Version = ' + version);
 
-['package', 'bower'].forEach(function (f) {
-    var meta = require('./' + f + '.json');
-    meta.version = version;
+['package', 'bower'].forEach(function(f) {
+  var meta = require('./' + f + '.json');
+  meta.version = version;
 
-    fs.writeFileSync(f + '.json', JSON.stringify(meta, null, '  ') + '\n');
-    sys.puts('Finished ' + f + '.json');
+  fs.writeFileSync(f + '.json', JSON.stringify(meta, null, '  ') + '\n');
+  sys.puts('Finished ' + f + '.json');
 });
 
-exec('webpack', function (error, stdout, stderr) {
-    sys.puts(stdout)
-});
+exec('webpack', function (error, stdout, stderr) { sys.puts(stdout) });
