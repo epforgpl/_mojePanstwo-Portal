ef601844edb05633be644567fac554b0ee586d2b
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2015-08-11 14:34:55 +0200

    Intro to docsedit

--- app/Model/Document.php
+++ app/Model/Document.php
@@ -4,7 +4,9 @@ class Document extends AppModel {
 
 	public $useDbConfig = 'mpAPI';
 	public $useTable = false;
-
+	
+	// function to load document content's package. if package === 0, then we are loading whole content.
+	
 	public function load($id, $package = 1) {
 
 		try {
