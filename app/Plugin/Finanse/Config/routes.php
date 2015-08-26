0d75e5905b3df201e7133208cfff852a5ce81b54
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2015-08-26 11:14:14 +0200

    Finanse

--- app/Plugin/Finanse/Config/routes.php
+++ app/Plugin/Finanse/Config/routes.php
@@ -1,22 +1,4 @@
-<?php
-
-Router::connect('/finanse/gminy/:gmina_id/budzet/:typ/dzialy/:id', array(
-    'plugin' => 'Finanse',
-    'controller' => 'Gminy',
-    'action' => 'dzial'
-), array(
-    'id' => '([0-9]+)',
-    'gmina_id' => '([0-9]+)',
-    'typ' => '(wydatki|dochody)',
-    'pass' => array('id', 'gmina_id', 'typ')
-));
-
-Router::connect('/finanse/gminy/:gmina_id/budzet/:typ/dzialy', array(
-    'plugin' => 'Finanse',
-    'controller' => 'Gminy',
-    'action' => 'dzialy'
-), array(
-    'gmina_id' => '([0-9]+)',
-    'typ' => '(wydatki|dochody)',
-    'pass' => array('gmina_id', 'typ')
-));
+<?
+	
+Router::connect('/finanse', array('plugin' => 'Finanse', 'controller' => 'Finanse', 'action' => 'view'));
+Router::connect('/finanse/:action', array('plugin' => 'Finanse', 'controller' => 'Finanse'));
