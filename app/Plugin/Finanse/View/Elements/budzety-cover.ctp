825a1869f08c0ce27ab29d394ec91fd5049d4d31
CainIsh <mariusz.machuta@gmail.com>
2015-08-31 15:31:47 +0200

    budżet - chart

--- app/Plugin/Finanse/View/Elements/budzety-cover.ctp
+++ app/Plugin/Finanse/View/Elements/budzety-cover.ctp
@@ -1,11 +1,17 @@
-<? $this->Combinator->add_libs('css', $this->Less->css('finanse', array('plugin' => 'Finanse'))); ?>
+<?
+$this->Combinator->add_libs('css', $this->Less->css('finanse', array('plugin' => 'Finanse')));
+
+$this->Combinator->add_libs('js', '../plugins/highstock/js/highstock');
+$this->Combinator->add_libs('js', '../plugins/highstock/locals');
+$this->Combinator->add_libs('js', 'Finanse.budzety');
+?>
 
 <div class="col-xs-12 col-md-2 dataAggsContainer">
-    
-    <ul class="dataAggs" style="opacity: 1;">
+
+    <ul class="dataAggs">
         <li class="agg">
             <div class="agg agg-List agg-Datasets">
-	            <ul class="nav nav-pills nav-stacked">           	            	
+                <ul class="nav nav-pills nav-stacked">
 	                <li>
 	                	<a href="/finanse">Start</a>
 	                </li>
@@ -22,15 +28,10 @@
 		    </div>
         </li>
     </ul>
-    
+
 </div>
 
 <div id="bdl_div" class="col-xs-12 col-md-10">
-
-	<div class="dataWrap">
-
-		<? debug($dataBrowser['aggs']['budzety']); ?>
-		
-	</div>
-
+    <div class="chart"
+         data-json='<?php echo json_encode($dataBrowser['aggs']['budzety']['top']['hits']['hits']); ?>'></div>
 </div>
---
+++ app/Plugin/Finanse/webroot/js/budzety.js
@@ -0,0 +1,118 @@
+$(function () {
+	var dataBlock = $('.chart'),
+		data = $.parseJSON(dataBlock.attr('data-json'));
+
+	function SortByDate(a, b) {
+		var aName = a.fields.source[0].data['budzety.rok'];
+		var bName = b.fields.source[0].data['budzety.rok'];
+		return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+	}
+
+	data.sort(SortByDate);
+
+	var dataPremier = [],
+		wyd = [],
+		doch = [],
+		def = [],
+		startDate = 1990,
+		denominacjaDate = 1996,
+		denominacjaValue = 10,
+		premierPlotBandData = {},
+		premierPlotBandColorO = 'rgba(200,200,200,.2)',
+		premierPlotBandColorE = 'rgba(150,150,150,.2)';
+
+	$.map(data, function (el) {
+		var self = el.fields.source[0].data;
+
+		console.log(self);
+
+		if (self['budzety.rok'] >= startDate) {
+			wyd.push({
+				id: self['prawo.id'],
+				x: self['budzety.rok'],
+				y: (self['budzety.rok'] < denominacjaDate) ? self['budzety.liczba_wydatki'] / denominacjaValue : self['budzety.liczba_wydatki']
+			});
+			doch.push({
+				id: self['prawo.id'],
+				x: self['budzety.rok'],
+				y: (self['budzety.rok'] < denominacjaDate) ? self['budzety.liczba_dochody'] / denominacjaValue : self['budzety.liczba_dochody']
+			});
+			def.push({
+				id: self['prawo.id'],
+				x: self['budzety.rok'],
+				y: (self['budzety.rok'] < denominacjaDate) ? self['budzety.liczba_deficyt'] / denominacjaValue : self['budzety.liczba_deficyt']
+			});
+
+			if (premierPlotBandData.id !== self['budzety.premier_czlowiek_id']) {
+				if (premierPlotBandData.id !== undefined) {
+					premierPlotBandData.to = self['budzety.rok'];
+					premierPlotBandData.color = (dataPremier.length % 2) ? premierPlotBandColorE : premierPlotBandColorO;
+					dataPremier.push(premierPlotBandData);
+					premierPlotBandData = {};
+				}
+				premierPlotBandData.id = self['budzety.premier_czlowiek_id'];
+				premierPlotBandData.label = {
+					align: 'left',
+					text: '<img src="//resources.sejmometr.pl/mowcy/a/3/' + premierPlotBandData.id + '.jpg" alt="" />',
+					useHTML: true,
+					y: +320
+				};
+				premierPlotBandData.from = self['budzety.rok'];
+			}
+		}
+	});
+
+	dataPremier.push(premierPlotBandData);
+	var dataSeries = [
+		{
+			type: 'line',
+			name: 'Wydatki',
+			data: wyd,
+			color: '#ff4643'
+		},
+		{
+			type: 'line',
+			name: 'Dochody',
+			data: doch,
+			setData: doch,
+			color: '#446ff9'
+		},
+		{
+			type: 'area',
+			name: 'Deficyt',
+			data: def,
+			color: '#fff700'
+		}];
+
+	dataBlock.highcharts({
+		chart: {
+			spacingBottom: 40
+		},
+		credits: {
+			enabled: false
+		},
+		legend: {
+			enabled: false
+		},
+		plotOptions: {
+			series: {
+				cursor: 'pointer',
+				events: {
+					click: function (event) {
+						window.location = "/dane/budzety/" + event.point.id;
+					}
+				},
+				shadow: true
+			}
+		},
+		series: dataSeries,
+		xAxis: {
+			plotBands: dataPremier,
+			pointStart: startDate,
+			pointInterval: 1
+		},
+		yAxis: {
+			title: ' '
+		}
+	});
+});
