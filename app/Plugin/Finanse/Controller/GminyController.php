5ab1fdc9d3a79d29c992ef16bd9f09a3db111ad9
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2015-09-04 14:33:20 +0200

    Finanse

--- app/Plugin/Finanse/Controller/GminyController.php
+++ app/Plugin/Finanse/Controller/GminyController.php
@@ -21,14 +21,75 @@ class GminyController extends AppController {
         
         $liczba_ludnosci = 750000;
 		$przedzial = array(
-			'min' => 500000,
-			'max' => 1000000,
+			'min' => 0,
+			'max' => 10000000,
 		);
+		
 		        
         $this->loadModel('Dane.Dataobject');
         $this->Dataobject->find('all', array(
 	        'limit' => 0,
 	        'aggs' => array(
+		        'gmina' => array(
+			        'filter' => array(
+				        'term' => array(
+					        'id' => $gmina_id,
+				        ),
+			        ),
+			        'aggs' => array(
+				        'wydatki' => array(
+					        'nested' => array(
+						        'path' => 'gminy-wydatki',
+					        ),
+					        'aggs' => array(
+						        'dzial' => array(
+							        'filter' => array(
+								        'bool' => array(
+									        'must' => array(
+										        array(
+											        'term' => array(
+												        'gminy-wydatki.rok' => 2014,
+											        ),
+										        ),
+										        array(
+											        'term' => array(
+												        'gminy-wydatki.kwartal' => 1,
+											        ),
+										        ),
+										        array(
+											        'term' => array(
+												        'gminy-wydatki.dzial_id' => $id,
+											        ),
+										        ),
+									        ),
+								        ),
+							        ),
+							        'aggs' => array(
+								        'rozdzialy' => array(
+									        'terms' => array(
+										        'field' => 'gminy-wydatki.rozdzial_id',
+										        'size' => 100,
+									        ),
+									        'aggs' => array(
+										        'nazwa' => array(
+											        'terms' => array(
+												        'field' => 'gminy-wydatki.rozdzial',
+												        'size' => 1,
+											        ),
+										        ),
+										        'wydatki' => array(
+											        'sum' => array(
+												        'field' => 'gminy-wydatki.wydatki',
+											        ),
+										        ),
+									        ),
+								        ),
+							        ),
+						        ),
+					        ),
+				        ),
+			        ),
+		        ),
 		        'gminy' => array(
 			        'filter' => array(
 				        'range' => array(
@@ -38,25 +99,97 @@ class GminyController extends AppController {
 					        ),
 				        ),
 			        ),
+			        'scope' => 'global',
 			        'aggs' => array(
 				        'wydatki' => array(
 					        'nested' => array(
-						        'path' => 'gminy-wydatki',
+						        'path' => 'gminy-wydatki-dzialy',
 					        ),
 					        'aggs' => array(
-						        'histogram' => array(
-							        'histogram' => array(
-								        'field' => 'suma_wydatki',
-								        'interval' => 10000,
-							        ),
-							        'aggs' => array(
-								        'suma_wydatki' => array(
-									        'sum' => array(
-										        'field' => 'wydatki',
+						        'timerange' => array(
+							        'filter' => array(
+								        'bool' => array(
+									        'must' => array(
+										        array(
+											        'term' => array(
+												        'gminy-wydatki-dzialy.rok' => 2014,
+											        ),
+										        ),
+										        /*
+										        array(
+											        'term' => array(
+												        'gminy-wydatki-dzialy.kwartal' => '0',
+											        ),
+										        ),
+										        */
+										        array(
+											        'term' => array(
+												        'gminy-wydatki-dzialy.id' => $id,
+											        ),
+										        ),
 									        ),
 								        ),
 							        ),
-						        ),						        
+							        'aggs' => array(
+								        'min' => array(
+									        'terms' => array(
+										        'field' => 'gminy-wydatki-dzialy.wydatki',
+										        'size' => '1',
+										        'order' => array(
+											        '_term' => 'asc',
+										        ),
+									        ),
+									        'aggs' => array(
+										        'reverse' => array(
+											        'reverse_nested' => '_empty',
+											        'aggs' => array(
+												        'top' => array(
+													        'top_hits' => array(
+														        'size' => 1,
+													        ),
+												        ),
+											        ),
+										        ),
+									        ),
+								        ),
+								        'max' => array(
+									        'terms' => array(
+										        'field' => 'gminy-wydatki-dzialy.wydatki',
+										        'size' => '1',
+										        'order' => array(
+											        '_term' => 'desc',
+										        ),
+									        ),
+									        'aggs' => array(
+										        'reverse' => array(
+											        'reverse_nested' => '_empty',
+											        'aggs' => array(
+												        'top' => array(
+													        'top_hits' => array(
+														        'size' => 1,
+													        ),
+												        ),
+											        ),
+										        ),
+									        ),
+								        ),
+								        'histogram' => array(
+									        'histogram' => array(
+										        'field' => 'gminy-wydatki-dzialy.wydatki',
+										        'interval' => 10000,
+									        ),
+									        'aggs' => array(
+										        'reverse' => array(
+											        'reverse_nested' => '_empty',
+											        'aggs' => array(
+												        
+											        ),
+										        ),
+									        ),
+								        ),
+								        
+							        ),
+								),
 					        ),
 				        ),
 			        ),
