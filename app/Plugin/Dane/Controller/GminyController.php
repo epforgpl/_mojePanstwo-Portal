67f3a20df2effbd85904b5eea243c1e6490811dd
Marek <marekbielecki93@gmail.com>
2015-10-22 15:10:25 +0200

    krakow hide aktywnosci

--- app/Plugin/Dane/Controller/GminyController.php
+++ app/Plugin/Dane/Controller/GminyController.php
@@ -54,10 +54,10 @@ class GminyController extends DataobjectsController
                     'id' => 'okregi',
                     'label' => 'Okręgi wyborcze',
                 ),
-                array(
+                /* array(
                     'id' => 'aktywnosci',
                     'label' => 'Aktywności radnych',
-                ),
+                ), */
             ),
         ),
         'urzad' => array(
