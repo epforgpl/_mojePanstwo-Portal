6519c37a192fe9b01c901cbd73c6aac087afa79c
Marek <marekbielecki93@gmail.com>
2015-11-19 11:46:15 +0100

    activities files

--- app/Plugin/Dane/Model/ActivitiesFiles.php
+++ app/Plugin/Dane/Model/ActivitiesFiles.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marek
- * Date: 18.11.15
- * Time: 16:57
- */
+
+class ActivitiesFiles extends AppModel {
+
+    public $useDbConfig = 'mpAPI';
+
+    public function getByActivity($activity_id) {
+        $res = $this->getDataSource()->request('activities/activities/getFiles/' . $activity_id, array(
+            'method' => 'GET'
+        ));
+
+        return $res;
+    }
+
+    public function getFile($activity_id, $file_id) {
+        $res = $this->getDataSource()->request('activities/activities/getFile/' . $activity_id . '/' . $file_id, array(
+            'method' => 'GET'
+        ));
+
+        return $res;
+    }
+
+}
