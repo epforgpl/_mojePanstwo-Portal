6f7c9ebe0b7c53d319380b5c11dc462a723c67f6
Cain-Ish <mariusz.machuta@gmail.com>
2015-12-03 09:29:22 +0100

    WPF - mapa układ

--- app/Plugin/Dane/webroot/js/krakow-wpf.js
+++ app/Plugin/Dane/webroot/js/krakow-wpf.js
@@ -5,17 +5,14 @@ $(document).ready(function () {
 		el = $wpfModal.find('.wpf-zoom');
 
 	$wpfModal.on('shown.bs.modal', function () {
-		var modalW = $wpfModal.find('.modal-body').width(),
-			w = el.width();
-
 		el.bigImage('init', {
 			zoom: {
-				width: modalW,
-				height: el.height() * (modalW / w),
+				width: el.width(),
+				height: el.height(),
 				maskElement: function () {
 					return $('<div/>', {
 						'class': 'wpf-zoom-mask',
-						'style': 'margin-left: -' + (w + ((modalW - w) / 2)) + 'px; margin-top: ' + el.height() + 'px;'
+						'style': 'margin-left: 1%;'
 					});
 				}
 			}
