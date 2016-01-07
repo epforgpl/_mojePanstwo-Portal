0bdc532fa6090162ab0fdc56eebabb0a6ebe22b8
CainIsh <mariusz.machuta@gmail.com>
2016-01-07 16:23:28 +0100

    PKRK

--- app/Plugin/Dane/webroot/js/view-gminy-krakow-aktywnosci.js
+++ app/Plugin/Dane/webroot/js/view-gminy-krakow-aktywnosci.js
@@ -82,53 +82,56 @@ $(document).ready(function () {
 					html += '<p>Łączna ilość punktów: ';
 					html += '<strong>' + number_format(c['*'], 0, '.', ' ') + '</strong>';
 					html += '</p>';
-					html += '<br /><p class="margin-top-5">zdobyte w: </p>';
 
-					if (typeof c.s !== "undefined") {
-						html += '<br /><p> - wystąpienia na posiedzeniu: ';
-						html += '<strong>' + number_format(c.s, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.v !== "undefined") {
-						html += '<br /><p> - oddane głosy: ';
-						html += '<strong>' + number_format(c.v, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.i !== "undefined") {
-						html += '<br /><p> - złożone interpelacje: ';
-						html += '<strong>' + number_format(c.i, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
+					if (parseInt(c['*'], 0) > 1) {
+						html += '<br /><p class="margin-top-5">zdobyte w: </p>';
 
-					if (typeof c.tel !== "undefined") {
-						html += '<br /><p> - udostępniony numer telefonu: ';
-						html += '<strong>' + number_format(c.tel, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.blog !== "undefined") {
-						html += '<br /><p> - prowadzenie bloga: ';
-						html += '<strong>' + number_format(c.blog, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.www !== "undefined") {
-						html += '<br /><p> - prowadzenie strony WWW: ';
-						html += '<strong>' + number_format(c.www, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.fb !== "undefined") {
-						html += '<br /><p> - prowadzenie konta Facebook: ';
-						html += '<strong>' + number_format(c.fb, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.twitter !== "undefined") {
-						html += '<br /><p> - prowadzenie konta Twitter: ';
-						html += '<strong>' + number_format(c.twitter, 0, '.', ' ') + '</strong>';
-						html += '</p>';
-					}
-					if (typeof c.email2 !== "undefined") {
-						html += '<br /><p> - udostępniony adresu e-mail: ';
-						html += '<strong>' + number_format(c.email2, 0, '.', ' ') + '</strong>';
-						html += '</p>';
+						if (typeof c.s !== "undefined") {
+							html += '<br /><p> - wystąpienia na posiedzeniu: ';
+							html += '<strong>' + number_format(c.s, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.v !== "undefined") {
+							html += '<br /><p> - oddane głosy: ';
+							html += '<strong>' + number_format(c.v, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.i !== "undefined") {
+							html += '<br /><p> - złożone interpelacje: ';
+							html += '<strong>' + number_format(c.i, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+
+						if (typeof c.tel !== "undefined") {
+							html += '<br /><p> - udostępniony numer telefonu: ';
+							html += '<strong>' + number_format(c.tel, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.blog !== "undefined") {
+							html += '<br /><p> - prowadzenie bloga: ';
+							html += '<strong>' + number_format(c.blog, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.www !== "undefined") {
+							html += '<br /><p> - prowadzenie strony WWW: ';
+							html += '<strong>' + number_format(c.www, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.fb !== "undefined") {
+							html += '<br /><p> - prowadzenie konta Facebook: ';
+							html += '<strong>' + number_format(c.fb, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.twitter !== "undefined") {
+							html += '<br /><p> - prowadzenie konta Twitter: ';
+							html += '<strong>' + number_format(c.twitter, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
+						if (typeof c.email2 !== "undefined") {
+							html += '<br /><p> - udostępniony adresu e-mail: ';
+							html += '<strong>' + number_format(c.email2, 0, '.', ' ') + '</strong>';
+							html += '</p>';
+						}
 					}
 
 					html += '</div>';
