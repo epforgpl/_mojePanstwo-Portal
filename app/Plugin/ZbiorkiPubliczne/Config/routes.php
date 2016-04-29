920cdc5fdf8ddeae9964bd57bb184cbbde93c6b9
CainIsh <mariusz.machuta@gmail.com>
2016-04-29 14:43:06 +0200

    datepicker

--- app/Plugin/ZbiorkiPubliczne/Config/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-Router::connect('/zbiorki_publiczne/formularz', array('plugin' => 'zbiorki_publiczne', 'controller' => 'zbiorki_publiczne', 'action' => 'formularz'));
-Router::connect('/zbiorki_publiczne/:action', array('plugin' => 'zbiorki_publiczne', 'controller' => 'zbiorki_publiczne'));
-Router::connect('/zbiorki_publiczne/*', array('plugin' => 'zbiorki_publiczne', 'controller' => 'zbiorki_publiczne'));
