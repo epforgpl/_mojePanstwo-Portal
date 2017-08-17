3b5e9541ced5182c36fce7746ca25850a2dae1e9
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2017-08-17 06:58:37 +0200

    Wyjazdy posłów

---
+++ app/Plugin/WyjazdyPoslow7/Config/routes.php
@@ -0,0 +1,2 @@
+<?php
+Router::connect('/wyjazdy_poslow7/*', array('plugin' => 'wyjazdy_poslow7', 'controller' => 'wyjazdy_poslow'));
--- app/Plugin/WyjazdyPosloww/Config/routes.php
+++
@@ -1,2 +0,0 @@
-<?php
-Router::connect('/wyjazdy_posloww/*', array('plugin' => 'wyjazdy_posloww', 'controller' => 'wyjazdy_poslow'));
